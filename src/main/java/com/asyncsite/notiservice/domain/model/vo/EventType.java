cdf9ed4061cf9e1b47722629d9c3ed2d63013595
KrongDev <leegeondev@gmail.com>
2025-08-10 13:58:16 +0900

    fix:  EventType Add(STUDY_APPROVED, PASSWORD_RESET)

--- src/main/java/com/asyncsite/notiservice/domain/model/vo/EventType.java
+++ src/main/java/com/asyncsite/notiservice/domain/model/vo/EventType.java
@@ -1,7 +1,13 @@
 package com.asyncsite.notiservice.domain.model.vo;
 
 public enum EventType {
+    // Study
     STUDY,
+    STUDY_APPROVED,
+
+    // Users
+    PASSWORD_RESET,
+
     NOTI,
     LOG,
     ACTION
